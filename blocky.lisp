f52ea2595896f7f897ac87bcec2731c590400d93
David O'Toole <dto@gnu.org>
2011-09-30 19:26:23 -0400

    yes

--- blocky.lisp
+++ blocky.lisp
@@ -39,7 +39,7 @@ stop *update-function* *default-world-axis-size* defsprite *target*
 *blocks* *buffer* *default-world-z-size* install-blocks shut-down
 later later-at later-when start-up seconds->frames keyboard-held-p
 keyboard-pressed-p holding-control *use-nominal-screen-size*
-keyboard-released-p *edit* callf with-font
+keyboard-released-p *edit* callf with-font *font*
 keyboard-time-in-current-state pretty-symbol-string *pointer-x*
 *pointer-y* keyboard-time-in-previous-state *updates* keyboard-down-p
 keyboard-keys-down keyboard-modifier-down-p keyboard-modifiers
