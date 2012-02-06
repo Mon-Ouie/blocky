cd9e6816adf8dddc4277a80c70de1f20884ac9e8
David O'Toole <dto@gnu.org>
2012-02-05 21:12:09 -0500

    update build script

--- build-unix.lisp
+++ build-unix.lisp
@@ -3,14 +3,14 @@
 (push (merge-pathnames "lib/" (values *default-pathname-defaults*))
       asdf:*central-registry*)
 
-(asdf:oos 'asdf:load-op 'xe2)
+(asdf:oos 'asdf:load-op 'blocky)
 
-(sb-ext:save-lisp-and-die "run-xiotank"
+(sb-ext:save-lisp-and-die "xalcyon"
 			  :toplevel (lambda ()
 				      (sb-posix:putenv
 				       (format nil "SBCL_HOME=~A" 
 					       #.(sb-ext:posix-getenv "SBCL_HOME")))
-				      (xe2:play "xiotank")
+				      (blocky:play "xalcyon")
 				      0)
 			  :executable t)
 
