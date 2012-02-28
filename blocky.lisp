bb11693fef0ff0f48983c11fca20ab6e467e3dbd
David O'Toole <dto@gnu.org>
2012-02-28 14:21:54 -0500

    removed obsolete symbols

--- blocky.lisp
+++ blocky.lisp
@@ -2,7 +2,7 @@
                
 ;; Copyright (C) 2006, 2007, 2008, 2009, 2010, 2011  David O'Toole
 
-;; Author: David O'Toole <dto@ioforms.org>
+;; Author: David O'Toole <dto@blxorms.org>
 ;; Keywords: multimedia, games
 ;; Version: 2.11
 
@@ -32,54 +32,44 @@
 (defpackage :blocky
     (:documentation "A visual multimedia programming language for Common Lisp.")
   (:use :common-lisp) 
-  (:export *default-frame-width* *default-frame-height* null-block
-*frequency* *output-chunksize* *output-channels* halt-sample *dt*
-*copyright-notice* *author* *message-hook-functions* add-to-list
-modify-joystick-profile defproject run start stop *update-function*
-*default-world-axis-size* defsprite *target* *blocks* *buffer*
-*default-world-z-size* install-blocks shut-down later later-at
+  (:export null-block *frequency* *output-chunksize* *output-channels*
+halt-sample *dt* *copyright-notice* *author* *message-hook-functions*
+add-to-list modify-joystick-profile defproject start stop
+*update-function* *target* *blocks* shut-down later later-at
 later-when start-up seconds->frames keyboard-held-p keyboard-pressed-p
 holding-control *scale-output-to-window* keyboard-released-p *edit*
-callf with-font *font* find-heading keyboard-time-in-current-state
+with-font *font* find-heading keyboard-time-in-current-state
 pretty-symbol-string *pointer-x* *pointer-y* is-joystick-event
 is-raw-joystick-event keyboard-time-in-previous-state *updates*
 keyboard-down-p keyboard-keys-down keyboard-modifier-down-p
 keyboard-modifiers draw-filled-circle draw-aa-circle get-keys
-*project-package-name* project-package-name make-block
-*form-command-handler-function* add-block remove-block with-quadtree
-*initialization-hook* initialize-engine hit-blocks quadtree-delete
-quadtree-insert build-quadtree quadtree-collide quadtree-show
-*quadtree* *quadtree-depth* split-string-on-lines message
-*prompt-sweden-keybindings* *prompt-qwerty-keybindings* *screen-width*
-transform-method-body *edit* roll-under make-stat make-universe
-initialize-colors *style* create-project *standard-categories*
+*project-package-name* project-package-name make-block add-block
+remove-block with-quadtree *initialization-hook* hit-blocks
+quadtree-delete quadtree-insert build-quadtree quadtree-collide
+quadtree-show *quadtree* *quadtree-depth* split-string-on-lines
+message *prompt-sweden-keybindings* *prompt-qwerty-keybindings*
+*screen-width* transform-method-body roll-under initialize-colors
+*style* load-project-image create-project-image *standard-categories*
 *left-turn* bind-event *right-turn* left-turn right-turn roll
 bind-event-to-method *colors* enable-key-repeat disable-key-repeat
 get-color define-method *default-font* field-value set-field-value
-object-fields dispatch-event run-project *user-init-file-name*
-distance icon-resource icon-image *directions*
-*clear-cached-images-on-resize* *opposites* find-resource-property
-compose-blank-fields font-width font-height *browser* browser
-set-browser find-object *windows* edit create
-transform-field-reference define-block *screen-height*
-formatted-line-width *last-event* formatted-line-height
-formatted-string-height formatted-string-width get-color create-image
-draw-image blocky edit define-prototype has-field *target* with-target
-defcell define-world *choose-direction-menu* set-field-options
+object-fields dispatch-event *user-init-file-name* distance
+icon-resource icon-image *directions* *opposites*
+find-resource-property compose-blank-fields font-width font-height
+find-object *windows* edit create blocky transform-field-reference
+define-block *screen-height* formatted-line-width
+formatted-line-height formatted-string-height formatted-string-width
+get-color create-image draw-image blocky edit define-prototype
+has-field *target* with-target define-world set-field-options
 *user-joystick-profile* field-option-value index-resource
 *default-joystick-profile* joystick-profile find-project-path
 index-project load-image-resource load-lisp-resource *executable*
-*function-buttons* *corner-buttons* *dance-arrows* *punctuation*
 *screen-height* player *screen-width* blockyp *nominal-screen-width*
 *nominal-screen-height* *gl-screen-width* *gl-screen-height*
-*dance-phrase-symbols* *dance-keybindings* *energy-dance-pad-mapping*
-*message-function* dash holding-shift *dark-target-arrow-images*
-get-button-index arrow-image message-to-standard-output
-reset-message-function *orthogonal-arrows* *diagonal-arrows*
-*function-buttons* *punctuation* *dance-arrows* *dance-pad-symbols*
-*make-prototype-id-package* arrow-formatted-string lturn rturn
-ticks-per-beat event-time event-arrow radian-angle
-draw-textured-rectangle default-project-directories *step-tolerance*
+*message-function* dash holding-shift get-button-index
+message-to-standard-output reset-message-function
+*make-prototype-id-package* lturn rturn ticks-per-beat radian-angle
+draw-textured-rectangle default-project-directories
 *resource-handlers* load-resource find-resource find-resource-object
 *colors* *world* make-directory-maybe load-user-init-file
 *project-directories* resource-to-plist *osx* *linux* make-resource
@@ -87,43 +77,36 @@ make-object-resource make-event *blocks* bind-event-to-text-insertion
 make-field-initializer clone make-field-initializer-body
 make-key-modifier-symbol make-key-string normalize-event make-keyword
 make-object queue-head queue-max queue-count *sender*
-field-reference-p null-next object-eq *message-send-symbol-suffix*
-*x11-color-data* object-name object-parent send send-super send-queue
-self opposite-direction object-address-string object step-in-direction
-direction-to plasma-rect subdivide-rect render-plasma add-hook
-run-hook queue-tail make-resource-link save-resource
-save-project-image system *system* *defined-resources* save-everything
-with-input-values with-inputs *export-formats* export-archive
-defresource export-application export-project make-queue queue unqueue
-queue-message queued-messages-p unqueue-message send-queue field-value
+field-reference-p null-next object-eq *x11-color-data* object-name
+object-parent send send-super send-queue self opposite-direction
+object-address-string object step-in-direction direction-to
+plasma-rect subdivide-rect render-plasma add-hook run-hook queue-tail
+make-resource-link save-resource save-project-image *system*
+*defined-resources* save-everything with-input-values with-inputs
+*export-formats* export-archive defresource export-application
+export-project make-queue queue unqueue queue-message
+queued-messages-p unqueue-message send-queue field-value
 random-direction random-choose *resources* load-font-resource
-save-object-resource initialize%super initialize%super
-/queue/initialize queue/initialize draw-string-solid read-iof
+save-object-resource initialize%super draw-string-solid read-box
 initialize-resource-table percent-of-time render-formatted-paragraph
 make-formatted-string draw-string-shaded set-blending-mode
 render-formatted-string render-formatted-line resource font-text-width
 write-sexp-to-file with-message-sender *message-sender*
-read-sexp-from-file with-fields with-field-values write-iof *grammar*
+read-sexp-from-file with-fields with-field-values write-blx *grammar*
 one-of left-hand-side right-hand-side expansions generate
 send-event-to-blocks play-music halt-music seek-music
 *joystick-mapping* play initialize-sound *generic-joystick-mapping*
-*ps3-joystick-mapping* *joystick-button-symbols* draw-resource-image
-*event-handler-function* *use-sound* trace-rectangle trace-row
-trace-column trace-octagon trace-line midpoint send-event self
-*project-blocks* defsprite get-some-object-name
-transform-declaration-field-descriptor show-blocks no-such-field
+*joystick-button-symbols* draw-resource-image *event-handler-function*
+*use-sound* midpoint send-event self get-some-object-name
+transform-declaration-field-descriptor no-such-field
 find-projects-in-directory goal directory-is-project-p
-find-all-projects *project* transform-tree *after-startup-hook*
-stat-value draw-line *default-message-verbosities*
-*message-verbosities* add-overlay set-message-verbosities
-operation-symbol message-symbol play-sample set-music-volume
-add-message-verbosities with-message-queue draw-pixel
-*user-keyboard-layout* *fullscreen* draw-circle set-field-option-value
-load-project field-options world set-frame-rate *frame-rate*
-*workbook* set-resource-modified-p *iof-file-extension* load-project
-*project* *project-path* *window-title* *window-position* restartably
-*default-shell-width* *default-shell-height* *system*
-set-timer-interval defgcell *message-logging* overlay
+find-directories find-all-projects *project* transform-tree
+*after-startup-hook* draw-line operation-symbol message-symbol
+play-sample set-music-volume draw-pixel *user-keyboard-layout*
+*fullscreen* draw-circle set-field-option-value load-project
+field-options world *frame-rate* set-resource-system-p
+*blx-file-extension* *project* *project-path* *window-title*
+*window-position* restartably *message-logging*
 joystick-axis-pressed-p joystick-axis-value joystick-axis-raw-value
 analog-stick-heading find-heading analog-stick-pressure
 *joystick-axis-size* *joystick-axis-dead-zone* *event-hook*
@@ -134,25 +117,16 @@ right-analog-stick-pressed-p right-analog-stick-pressure
 initialize-console joystick-axis-value poll-joystick-button
 joystick-button-state reset-joysticks *device-profiles*
 button-to-symbol symbol-to-button find-device-profile set-screen-width
-*universe* *play-args* set-screen-height genseq *zoom-factor*
-zoom-image is-zoomed-resource *timer-interval* save-objects
-enable-timer disable-timer while defmission send-to-blocks
-enable-held-keys disable-held-keys do-cells draw-box *resizable*
-achieve *resize-hook* draw-rectangle *quitting* playerp
+*play-args* set-screen-height genseq save-objects enable-timer
+draw-box *resizable* *resize-hook* draw-rectangle playerp
 *after-load-project-hook* *mission* mission-variable find-bounding-box
-combine-worlds stack-vertically set-mission-variable horizontal-extent
+combine stack-vertically set-mission-variable horizontal-extent
 vertical-extent flip-horizontally flip-vertically mirror-horizontally
 mirror-vertically world with-mission-locals with-empty-world
 define-turtle stack-horizontally *background-color* combine-beside
-combine-below set-sample-callback set-music-callback cffi-chunk-buffer
-convert-cffi-sample get-sample-buffer register-sample-generator
-register-voice unregister-voice register-voice-mixer mix-voices
-convert-cffi-sample-to-internal *block* *buffer* define-turtle
-convert-internal-sample-to-cffi get-ticks block-variable *block-font*
-with-block-variables set-block-variable with-blocks
-with-mission-locals *project* quit reset seek-music make-keyword
-object field-value make-queue find-parent set-field-value find-super
-*font* set-field-options field-options field-documentation
+combine-below get-ticks *block-font* quit reset seek-music
+make-keyword object field-value make-queue find-parent set-field-value
+find-super *font* set-field-options field-options field-documentation
 set-field-option-value field-option-value *lookup-failure*
 no-such-field has-field has-method send send-queue send-super
 serialize deserialize initialize-method-cache *send-super-depth*
@@ -164,7 +138,7 @@ queued-messages-p with-field-values with-fields-ex unqueue-message
 unqueue-and-send-message with-message-queue message-symbol
 operation-symbol *sender* message-reader transform-tree
 field-reference-p transform-field-reference transform-method-body
-object-parent object-name $ object-fields define-method
+object-parent object-name object-fields define-method
 *joystick-dead-zone* define-prototype new object-p self percent-gray
 percent-grey *indicators* find-indicator-texture draw-indicator
 font-text-width transform-declaration-field-descriptor is-a
