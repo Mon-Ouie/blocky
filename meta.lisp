703cb32b93945a96c4ef5af076f023103a321e82
David O'Toole <dto@gnu.org>
2012-03-15 10:53:08 -0400

    whitespace

--- meta.lisp
+++ meta.lisp
@@ -18,6 +18,7 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+
 (in-package :blocky)
 
 ;;; Send the messages in a list to the referent of the first element
