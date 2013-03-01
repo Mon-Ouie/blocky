d46199cdab84a9f115a0a91810d893844fdfda91
dto <dto@ioforms.org>
2013-03-01 16:15:26 -0500

    fix build

--- build-unix.lisp
+++ build-unix.lisp
@@ -1,9 +1,17 @@
 (require 'sb-posix)
 
-(push (merge-pathnames "lib/" (values *default-pathname-defaults*))
+(push (merge-pathnames "lib/" *default-pathname-defaults*)
       asdf:*central-registry*)
 
-(push #p"~/2x0ng/" asdf:*central-registry*)
+;; (let ((quicklisp-init (merge-pathnames "quicklisp/setup.lisp"
+;;                                        (user-homedir-pathname))))
+;;   (when (probe-file quicklisp-init)
+;;     (load quicklisp-init)))
+(ql:quickload '(:lispbuilder-sdl-mixer :lispbuilder-sdl-ttf :lispbuilder-sdl-image :uuid :cl-opengl :cl-fad))
+(asdf:load-system :blocky)
+
+
+(push #p"/home/dto/2x0ng/" asdf:*central-registry*)
 
 (asdf:oos 'asdf:load-op '2x0ng)
 
@@ -11,7 +19,7 @@
 			  :toplevel (lambda ()
 				      (sb-posix:putenv
 				       (format nil "SBCL_HOME=~A" 
-					       #.(sb-ext:posix-getenv "SBCL_HOME")))
+				      	       #.(sb-ext:posix-getenv "SBCL_HOME")))
 				      (setf blocky::*executable* t)
 				      (2x0ng:2x0ng)
 				      0)
